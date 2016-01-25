b1380932c5ef9ea94d3b5ea297c9bf4dcfeead61
Brad Saund <brad.saund@gmail.com>
2016-01-25 16:14:34 -0500

    Added some comments and minor cleanup

--- src/gazebo_ray_trace/src/rayTracePlugin.cpp
+++ src/gazebo_ray_trace/src/rayTracePlugin.cpp
@@ -25,19 +25,9 @@ namespace gazebo
     {
     }
 
-    bool dist(gazebo_ray_trace::RayTrace::Request &req,
-    	      gazebo_ray_trace::RayTrace::Response &resp)
-    {
-      resp.dist = sqrt(
-    		       pow(req.start.x - req.end.x, 2) + 
-    		       pow(req.start.y - req.end.y, 2) + 
-    		       pow(req.start.z - req.end.z, 2));
-
-      ROS_INFO("Sending Response From Within Gazebo: %f", resp.dist);
-      return true;
-    }
-
-
+    /**
+     *  Performs ray tracing of a ros request in the loaded world.
+     */
     bool rayTrace(gazebo_ray_trace::RayTrace::Request &req,
     	      gazebo_ray_trace::RayTrace::Response &resp)
     {
@@ -58,24 +48,17 @@ namespace gazebo
       ray_->GetIntersection(dist, entityName);
 
       resp.dist = dist;
-      ROS_INFO("Sending Response From Within Gazebo: %ld", (long int)resp.dist);
+
+      ROS_INFO("Traced ray and responded with distance: %f", resp.dist);
       return true;
     }
 
     void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf){
-      // ros::init(argc, argv, "ray_trace_server");
-      // ros::NodeHandle n;
-
-      // ros::ServiceServer service = n.advertiseService("ray_trace", &RayTracer.rayTrace);
-      // ROS_INFO("Ready to ray trace");
-      // ros::spin();
 
       if(!ros::isInitialized()){
       	ROS_FATAL_STREAM("A ROS node for Gazebo has not been initialized, unable to load plugin");
       }
       
-      // this->deferred_load_thread_ = boost::thread(
-      // 						  boost::bind(&RayTracer::LoadThread, this));
       this->initStructures(_world);
       this->advertiseServices();
       
@@ -83,13 +66,15 @@ namespace gazebo
 	  
     }
 
+    /**
+     *  Advertise the ray tracing service.
+     */
     void advertiseServices()
     {
       std::string robot_namespace = "gazebo_simulation";
       this->rosnode_ = new ros::NodeHandle(robot_namespace);
+
       // this->srv_ = this->rosnode_->advertiseService("ray_trace", &RayTracer::rayTrace);
-      // this->srv_ = this->rosnode_->
-      // 	advertiseService("ray_trace", boost::bind(&RayTracer::rayTrace, this, _1, _2));
       ros::AdvertiseServiceOptions ray_trace_srv = 
       	ros::AdvertiseServiceOptions::create<gazebo_ray_trace::RayTrace>
       	("ray_trace",
@@ -98,6 +83,9 @@ namespace gazebo
       this->srv_ = this->rosnode_->advertiseService(ray_trace_srv);
     }
 
+    /** 
+     *  Creates a ray using the world's physics engine.
+     */
     void initStructures(physics::WorldPtr _world)
     {
       gazebo::physics::PhysicsEnginePtr engine = _world->GetPhysicsEngine(); 
@@ -105,8 +93,6 @@ namespace gazebo
 	(engine->CreateShape("ray", gazebo::physics::CollisionPtr()));
 
     }
-
-
 
   };
   GZ_REGISTER_WORLD_PLUGIN(RayTracer);

12b38848cf949bfd173e71863e94e66419f28ae4
Brad Saund <brad.saund@gmail.com>
2016-01-29 11:28:02 -0500

    Model and broadcast particles as transformations

--- src/using_markers/src/plotParticles.cpp
+++ src/using_markers/src/plotParticles.cpp
@@ -1,24 +1,32 @@
 #include <ros/ros.h>
 #include <visualization_msgs/MarkerArray.h>
 #include <cmath>
-#include "simpleShape.h"
 #include <tf/transform_broadcaster.h>
 #include <geometry_msgs/Transform.h>
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/PoseArray.h>
 #include <random>
 
 class ShapePlotter
 {
  private:
   ros::NodeHandle n;
-  ros::Publisher marker_pub;
+
 
   tf::TransformBroadcaster br;
+  ros::Publisher particle_pub;
+  ros::Publisher marker_pub;
+
   tf::Transform transform;
   visualization_msgs::MarkerArray points;
+  geometry_msgs::PoseArray particles_;
+
+  int numParticles = 50;
 
  public:
   ShapePlotter();
-  void generateParticles();
+  void updateMarkers();
+  void generateTransforms();
   void plotParticles();
 };
 
@@ -27,35 +35,49 @@ class ShapePlotter
 ShapePlotter::ShapePlotter()
 {
   marker_pub = n.advertise<visualization_msgs::MarkerArray>("visualization_marker_array", 10);
+  particle_pub = n.advertise<geometry_msgs::PoseArray>("tranform_particles", 10);
 }
 
-
-void ShapePlotter::generateParticles()
+/** 
+ *  Generates the particles in random configureation
+ */
+void ShapePlotter::generateTransforms()
 {
-  int numMarkers = 50;
-
   std::random_device rd;
   std::mt19937 gen(rd());
   std::normal_distribution<> randn(0, 1);
+  
+  particles_.poses.resize(numParticles);
+  for(int i=0; i<numParticles; i++){
+    geometry_msgs::Pose particleTransform;
+    particleTransform.position.x = randn(gen)/50;
+    particleTransform.position.y = randn(gen)/50;
+    particleTransform.position.z = randn(gen)/50;
+
+    particleTransform.orientation = 
+      tf::createQuaternionMsgFromRollPitchYaw(randn(gen)/50, randn(gen)/50, randn(gen)/50);
+    
+
+    // particles_.push_back(particleTransform);
+    particles_.poses[i] = particleTransform;
+  }
+  particle_pub.publish(particles_);
+}
 
 
- 
-
-
-  points.markers.resize(numMarkers);
-  for(int i=0; i<numMarkers; i++){
+/** 
+ *  Update markers according to their particle transforms
+ */
+void ShapePlotter::updateMarkers()
+{
+  points.markers.resize(particles_.poses.size());
+  for(int i=0; i<particles_.poses.size(); i++){
     points.markers[i].header.frame_id = "particle_frame";
     points.markers[i].header.stamp = ros::Time::now();
     points.markers[i].ns = "points_and_lines";
     points.markers[i].action = visualization_msgs::Marker::ADD;
 
-    points.markers[i].pose.position.x = 0.0 + randn(gen)/50;
-    points.markers[i].pose.position.y = 0.0 + randn(gen)/50;
-    points.markers[i].pose.position.z = 0.0 + randn(gen)/50;
-
-    // points.markers[i].pose.orientation.w = 1.0;
-    points.markers[i].pose.orientation = 
-      tf::createQuaternionMsgFromRollPitchYaw(randn(gen)/50, randn(gen)/50, randn(gen)/50);
+    points.markers[i].pose = particles_.poses[i];
 
     points.markers[i].id = i;
 
@@ -64,17 +86,17 @@ void ShapePlotter::generateParticles()
 
 
     // POINTS markers use x and y scale for width/height respectively
+    // Boeing part is in inches, we are in meters
     points.markers[i].scale.x = .0254;
     points.markers[i].scale.y = .0254;
     points.markers[i].scale.z = .0254;
 
 
-    // %Tag(COLOR)%
-    // Points are green
     points.markers[i].color.r = 0.5f;
     points.markers[i].color.g = 0.7f;
     points.markers[i].color.b = 0.7f;
 
+    //alpha to make the particles transparent
     points.markers[i].color.a = 0.1;
 
   }
@@ -93,17 +115,11 @@ void ShapePlotter::plotParticles(){
   tf::transformStampedTFToMsg(tfstmp, trans);
 
   br.sendTransform(trans);
-  int numMarkers = 50;
 
-
-  for(int i=0; i<numMarkers; i++){
-
+  for(int i=0; i<particles_.poses.size(); i++){
     points.markers[i].header.stamp = ros::Time::now();
-
   }
-
- 
- 
+  
   marker_pub.publish(points);
 }
 
@@ -114,27 +130,15 @@ int main(int argc, char **argv)
 
   ShapePlotter plt;
 
-  // Cube c;
-
-  // tf::Transform transform;
   ros::Duration waitForRViz(1.0);
-  plt.generateParticles();
+
+  plt.generateTransforms();
+  plt.updateMarkers();
 
 
   while (ros::ok()) {
     waitForRViz.sleep();
     plt.plotParticles();
-
-    // double t = ros::Time::now().toSec();
-    // transform.setOrigin( tf::Vector3(2.0*sin(t),
-    // 				     2.0*cos(t),
-    // 				     0.0) );
-    // tf::Quaternion q;
-    // q.setRPY(t,0,0);
-    // transform.setRotation(q);
-
-    // plt.plotShape(c, tf::StampedTransform(transform, ros::Time::now(), "my_frame", "newFrame1"));
-    // ros::spinOnce();
   }
   
   

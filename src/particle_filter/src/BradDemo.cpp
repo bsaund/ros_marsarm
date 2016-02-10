7b0de71c91fe3b75a0d8a2b7e38af9945fa52edb
Brad Saund <brad.saund@gmail.com>
2016-02-09 21:03:39 -0500

    using plotRayUtils from different package

--- src/particle_filter/src/BradDemo.cpp
+++ src/particle_filter/src/BradDemo.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 #include <gazebo_ray_trace/calcEntropy.h>
+#include <gazebo_ray_trace/plotRayUtils.h>
 #include <vector>
+#include <tf/tf.h>
 
-int main()
+
+int main(int argc, char **argv)
 {
+  ros::init(argc, argv, "ray_trace_test");
+  PlotRayUtils plt;
 
-  std::vector<double> bla;
-  std::vector<double> dist;
-  for(int i=0; i< 50; i++){
-    bla.push_back(i + 0.01);
-  }
-  
-  std::cout<< CalcEntropy::calcEntropy(bla);
+  plt.plotEntropyRay(tf::Point(1.5, 2, 3.5),
+		     tf::Point(1.5, 2, 2.5),
+		     false);
+  plt.plotEntropyRay(tf::Point(1.5, 3, 3.5),
+		     tf::Point(1.5, 1, 2.5),
+		     false);
 
   std::cout << "Hello World" << std::endl;
   return 0;

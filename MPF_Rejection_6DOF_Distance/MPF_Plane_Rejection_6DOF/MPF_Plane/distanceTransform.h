32b0c60c643da270195e56124d1791b22641cfef
ShiyuanChen <chenshiyuan122@gmail.com>
2016-02-17 19:52:42 -0500

    update subspace distance transform

--- MPF_Rejection_6DOF_Distance/MPF_Plane_Rejection_6DOF/MPF_Plane/distanceTransform.h
+++ MPF_Rejection_6DOF_Distance/MPF_Plane_Rejection_6DOF/MPF_Plane/distanceTransform.h
@@ -11,20 +11,35 @@ public:
 	int grid_nums[3];
 	double ***dist_transform;
 	double ***obstacle_map;
-	distanceTransform(double world_range[3][2], double grid_res);
-	void build(double cube_para[3]);
+	distanceTransform(double range, double grid_res);
+	void build(double cube_para[3], double World_Range[3][2]);
 protected:
 	void distanceTransform_1D(double ***dist_transform_1D, int range[3], double ***cost_fun, int dir, int idx1, int idx2);
 };
-distanceTransform::distanceTransform(double World_Range[3][2], double Grid_Res) :grid_res(Grid_Res) {
+/*distanceTransform::distanceTransform(double World_Range[3][2], double Grid_Res) :grid_res(Grid_Res) {
 	memcpy(world_range, World_Range, 6 * sizeof(double));
+}*/
+distanceTransform::distanceTransform(double range, double grid_Res): grid_res(grid_Res) {
+	for (int i = 0; i < 3; i++) {
+		grid_nums[i] = ceil(range / grid_res);
+	}
+	dist_transform = new double **[grid_nums[0]];
+	obstacle_map = new double **[grid_nums[0]];
+	for (int i = 0; i < grid_nums[0]; i++) {
+		dist_transform[i] = new double *[grid_nums[1]];
+		obstacle_map[i] = new double *[grid_nums[1]];
+		for (int j = 0; j < grid_nums[1]; j++) {
+			dist_transform[i][j] = new double[grid_nums[2]];
+			obstacle_map[i][j] = new double[grid_nums[2]];
+		}
+	}
 }
 void distanceTransform::distanceTransform_1D(double ***dist_transform_1D, int range[3], double ***cost_fun, int dir, int idx1, int idx2)
 {
 	int k = 0;
 	int *envelope = new int[range[dir]];
 	envelope[0] = 0;
-	double *bound = new double[range[dir]];
+	double *bound = new double[range[dir] + 1];
 	bound[0] = -std::numeric_limits<double>::infinity();
 	bound[1] = std::numeric_limits<double>::infinity();
 	double s;
@@ -95,25 +110,14 @@ void distanceTransform::distanceTransform_1D(double ***dist_transform_1D, int ra
 			dist_transform_1D[idx1][idx2][q] = double((q - envelope[k]) * (q - envelope[k])) + cost_fun[idx1][idx2][envelope[k]];
 		}
 	}
-	
+	delete[] envelope;
+	delete[] bound;
 }
-void distanceTransform::build(double cube_para[3])
+void distanceTransform::build(double cube_para[3], double World_Range[3][2])
 {
 	double LARGE_NUM = 10000000;
-	for (int i = 0; i < 3; i++) {
-		grid_nums[i] = ceil((world_range[i][1] - world_range[i][0]) / grid_res);
-	}
-	dist_transform = new double **[grid_nums[0]];
-	obstacle_map = new double **[grid_nums[0]];
-	for (int i = 0; i < grid_nums[0]; i++) {
-		dist_transform[i] = new double *[grid_nums[1]];
-		obstacle_map[i] = new double *[grid_nums[1]];
-		for (int j = 0; j < grid_nums[1]; j++) {
-			dist_transform[i][j] = new double[grid_nums[2]];
-			obstacle_map[i][j] = new double[grid_nums[2]];
-		}
-	}
 	double x, y, z;
+	memcpy(world_range, World_Range, 6 * sizeof(double));
 	for (int i = 0; i < grid_nums[0]; i++) {
 		for (int j = 0; j < grid_nums[1]; j++) {
 			for (int k = 0; k < grid_nums[2]; k++) {
@@ -145,15 +149,6 @@ void distanceTransform::build(double cube_para[3])
 			}
 		}
 	}
-	//for (int k = 0; k < grid_nums[2]; k++) {
-	//	for (int i = 0; i < grid_nums[0]; i++) {
-	//		for (int j = 0; j < grid_nums[1]; j++) {
-	//			cout << dist_transform[i][j][k] << "  ";
-	//		}
-	//		cout << endl;
-	//	}
-	//	cout << endl;
-	//}
 }
 
 #endif // DISTANCE_TRANSFORM_H

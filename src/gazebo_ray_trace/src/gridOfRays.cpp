a2f336b3579c2db65637023acd297f8d193f23e6
Brad Saund <brad.saund@gmail.com>
2016-02-19 16:56:07 -0500

    adjusted sleep constant to be faster

--- src/gazebo_ray_trace/src/gridOfRays.cpp
+++ src/gazebo_ray_trace/src/gridOfRays.cpp
@@ -39,7 +39,7 @@ int main(int argc, char **argv){
       // s  << std::fixed << std::setprecision(2) << entropy;
       // plt.labelRay(start, s.str());
       plt.plotCylinder(start, end, 0.01, 0.002);
-      ros::Duration(0.2).sleep();
+      ros::Duration(0.1).sleep();
     } 
   }
 

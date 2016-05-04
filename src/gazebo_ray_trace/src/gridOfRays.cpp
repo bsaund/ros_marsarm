9fcec78f7fe217548ac5b886f6900e0ea8849eee
Brad Saund <brad.saund@gmail.com>
2016-05-04 15:36:13 -0400

    Altered gridOfRays bounds for actual rendering

--- src/gazebo_ray_trace/src/gridOfRays.cpp
+++ src/gazebo_ray_trace/src/gridOfRays.cpp
@@ -10,6 +10,7 @@
 #include <math.h>
 #include <iostream>
 #include <fstream>
+#include <string.h>
 
 
 
@@ -18,66 +19,53 @@ int main(int argc, char **argv){
   ros::init(argc, argv, "ray_trace_grid");
 
   std::ofstream myfile;
-  myfile.open("example.txt");
-
 
 
   PlotRayUtils plt;
-  double y, z;
-  y = -.9;
-  while( y <= .9){
-    z = 0.0;
-    while( z <= .6){
-      tf::Point start(0, y, z);
-      tf::Point end(2, y, z);
 
-      // plt.plotCylinder(start, end, 0.01, 0.002);
-      // ros::Duration(0.4).sleep();
 
-      myfile << y << ", " << z << ", " << plt.getIG(start, end, 0.01, 0.002) << std::endl;
-      z += .01;
+  tf::Transform t, R;
+  t.setOrigin(tf::Vector3(.9,0,.3));
+  t.setRotation(tf::createQuaternionFromRPY(0,0,0));
+
+
+
+
+  double x, y, angle;
+  angle = 0;
+  while(angle <= 1.57){
+    std::cout << "Angle: " << angle << std::endl;
+
+    std::ostringstream ss;
+    ss << "XY_rotation_" << angle << ".txt"; 
+    myfile.open(ss.str());
+    R.setRotation(tf::createQuaternionFromRPY(0,-angle,0));
+    x = 0.3;
+    while( x <= 1.4){
+      y = -.9;
+      while( y <= .9){
+	tf::Point start(x, y, 1.0);
+	tf::Point end(x, y, 0.0);
+
+	start = t * R * t.inverse() * start;
+	end   = t * R * t.inverse() * end;
+
+	// plt.plotCylinder(start, end, 0.01, 0.002);
+	// plt.plotRay(start, end, false);
+	// ros::Duration(0.1).sleep();
+
+	myfile << x << ", " << y << ", " << plt.getIG(start, end, 0.01, 0.002) << std::endl;
+	y += .01;
+      }
+      std::cout << "x, y: " << x << ", " << y << std::endl;
+      x += .01;
     }
-    std::cout << "x, z: " << y << ", " << z << std::endl;
-    y += .01;
+    
+    angle += .1;
+    myfile.close();
   }
 
-  // double x, z;
-  // x = -.3;
-  // while( x <= 1.3){
-  //   z = 0.0;
-  //   while( z <= .6){
-  //     tf::Point start(x, -1, z);
-  //     tf::Point end(x, 1, z);
 
-  //     // plt.plotCylinder(start, end, 0.01, 0.002);
-  //     // ros::Duration(0.4).sleep();
-
-  //     myfile << x << ", " << z << ", " << plt.getIG(start, end, 0.01, 0.002) << std::endl;
-  //     z += .01;
-  //   }
-  //   std::cout << "x, z: " << x << ", " << z << std::endl;
-  //   x += .01;
-  // }
-
-  // double x, y;
-  // x = -.3;
-  // while( x <= 1.3){
-  //   y = -.9;
-  //   while( y <= .9){
-  //     tf::Point start(x, y, 1.0);
-  //     tf::Point end(x, y, 0.0);
-
-  //     // plt.plotCylinder(start, end, 0.01, 0.002);
-  //     // ros::Duration(0.4).sleep();
-
-  //     myfile << x << ", " << y << ", " << plt.getIG(start, end, 0.01, 0.002) << std::endl;
-  //     y += .01;
-  //   }
-  //   std::cout << "x, y: " << x << ", " << y << std::endl;
-  //   x += .01;
-  // }
-
-  myfile.close();
   // for(int i = -2; i < 3; i++){
   //   for(int j = -2; j < 3; j++){
   //     double x = .5 + 0.2 * (i);

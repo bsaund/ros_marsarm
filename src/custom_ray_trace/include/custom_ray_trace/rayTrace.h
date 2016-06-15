6f4366efcb4e8580f6eace6ad2adaed5f0911b1b
Brad Saund <brad.saund@gmail.com>
2016-06-15 16:48:15 -0400

    clean up rayTrace.h: Shorter, faster, better

--- src/custom_ray_trace/include/custom_ray_trace/rayTrace.h
+++ src/custom_ray_trace/include/custom_ray_trace/rayTrace.h
@@ -43,12 +43,14 @@ using namespace std;
 
 
 #define EPSILON 0.000001
-int intersect_triangle(array<double,3> orig, array<double,3> dir,
-		       double vert0[3], double vert1[3], double vert2[3],
+int intersect_triangle(const array<double,3> &orig, const array<double,3> &dir,
+		       const double vert0[3], 
+		       const double vert1[3], 
+		       const double vert2[3],
 		       double &t, double &u, double &v)
 {
   double edge1[3], edge2[3], tvec[3], pvec[3], qvec[3];
-  double det, inv_det;
+  double det;
   /* find vectors for two edges sharing vert0 */
   SUB(edge1, vert1, vert0);
   SUB(edge2, vert2, vert0);
@@ -63,48 +65,41 @@ int intersect_triangle(array<double,3> orig, array<double,3> dir,
   SUB(tvec, orig, vert0);
 
 
-  if (det > EPSILON)
-    {
-      /* calculate U parameter and test bounds */
-      u = DOT(tvec, pvec);
-      if (u < 0.0 || u > det)
-	return 0;
+  if (det > EPSILON) {
+    /* calculate U parameter and test bounds */
+    u = DOT(tvec, pvec);
+    if (u < 0.0 || u > det)
+      return 0;
 
-      /* prepare to test V parameter */
-      CROSS(qvec, tvec, edge1);
+    /* prepare to test V parameter */
+    CROSS(qvec, tvec, edge1);
 
-      /* calculate V parameter and test bounds */
-      v = DOT(dir, qvec);
-      if (v < 0.0 || u + v > det)
-	return 0;
+    /* calculate V parameter and test bounds */
+    v = DOT(dir, qvec);
+    if (v < 0.0 || u + v > det)
+      return 0;
 
-    }
-  else if (det < -EPSILON)
-    {
-      /* calculate U parameter and test bounds */
-      u = DOT(tvec, pvec);
-      if (u > 0.0 || u < det)
-	return 0;
+  }
+  else if (det < -EPSILON) {
+    /* calculate U parameter and test bounds */
+    u = DOT(tvec, pvec);
+    if (u > 0.0 || u < det)
+      return 0;
 
-      /* prepare to test V parameter */
-      CROSS(qvec, tvec, edge1);
+    /* prepare to test V parameter */
+    CROSS(qvec, tvec, edge1);
 
-      /* calculate V parameter and test bounds */
-      v = DOT(dir, qvec);
-      if (v > 0.0 || u + v < det)
-	return 0;
-    }
+    /* calculate V parameter and test bounds */
+    v = DOT(dir, qvec);
+    if (v > 0.0 || u + v < det)
+      return 0;
+  }
   else return 0;  /* ray is parallell to the plane of the triangle */
 
   /* calculate t, ray intersects triangle */
-  inv_det = 1.0 / det;
-  t = DOT(edge2, qvec) * inv_det;
-  if (t < 0)
-    return 0;
-  /*(*u) *= inv_det;
-    (*v) *= inv_det;*/
+  t = DOT(edge2, qvec) / det;
 
-  return 1;
+  return t>0;
 }
 
 
@@ -121,28 +116,14 @@ int intersect_triangle(array<double,3> orig, array<double,3> dir,
 int getIntersection(stl::Mesh mesh, array<double,3> pstart, 
 		    array<double,3> dir, double &distToPart)
 {
-
-  int num_mesh = int(mesh.size());
-  double vert0[3], vert1[3], vert2[3];
-  double t;
-  double u;
-  double v;
+  double t, u, v;
   double tMin = 100000;
 
-
-  for (int i = 0; i < num_mesh; i++) {
-
-    vert0[0] = mesh[i][1][0];
-    vert0[1] = mesh[i][1][1];
-    vert0[2] = mesh[i][1][2];
-    vert1[0] = mesh[i][2][0];
-    vert1[1] = mesh[i][2][1];
-    vert1[2] = mesh[i][2][2];
-    vert2[0] = mesh[i][3][0];
-    vert2[1] = mesh[i][3][1];
-    vert2[2] = mesh[i][3][2];
-    if (intersect_triangle(pstart, dir, vert0, vert1, vert2, t, u, v) == 1 && t < tMin){
-      tMin = t;
+  for (stl::vec4x3 face : mesh) {
+    if (intersect_triangle(pstart, dir, 
+			   &face[1][0], &face[2][0], &face[3][0], 
+			   t, u, v)){
+      tMin = std::min(t, tMin);
     }
   }
 
